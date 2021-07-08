d20e119945a7b0393cbbf9281e2689669a70f9d3
Hien Pham <phamthehien120@gmail.com>
2021-07-08 15:51:45 -0700

    'baiTapES6FormKhaiBaoYTe'

---
+++ app/models/ThongTin.js
@@ -0,0 +1,45 @@
+export class ThongTin {
+  ho = "";
+  ten = "";
+  email = "";
+  soDienThoai = "";
+  diaChi = "";
+  thongTinDiChuyen = "";
+  loaiNhiem = "";
+
+  constructor() {}
+
+  hienThiThongTin() {
+    let content = `
+          <tr>
+              <th>Họ & tên</th>
+              <td>${this.ho} ${this.ten}</td>
+          </tr>
+          <tr>
+              <th>Email</th>
+              <td>${this.email}</td>
+          </tr>
+          <tr>
+              <th>Số điện thoại</th>
+              <td>${this.soDienThoai}</td>
+          </tr>
+          <tr>
+              <th>Ngày tháng năm sinh</th>
+              <td>${this.ngay}/${this.thang}/${this.nam}</td>
+          </tr>
+          <tr>
+              <th>Địa chỉ</th>
+              <td>${this.diaChi}</td>
+          </tr>
+          <tr>
+              <th>Thông tin di chuyển</th>
+              <td>${this.thongTinDiChuyen}</td>
+          </tr>
+          <tr>
+              <th>Loại tiếp xúc</th>
+              <td>${this.loaiNhiem}</td>
+          </tr>
+      `;
+    return content;
+  }
+}
